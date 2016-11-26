34fe6bfc6809cc4a695cb2ff58d1364947d1ed84
binge <gebinsdyz@163.com>
2016-11-26 15:47:48 +0800

    添加webpack，支持本地服务

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # demo-for-shasha
+
+开始
+1）安装node v6.3.1
+
+2）安装webpack和webpack-dev-server
+npm install  
+
+3）启动服务器
+npm start
+
+4）在浏览器输入
+http://localhost:1314
+
+5）继续开发
--- js/index.js
+++ js/index.js
@@ -1,5 +1,16 @@
+/**
+ * ------------------------------------------------------------------
+ * 首页
+ * ------------------------------------------------------------------
+ */
+
 $(function(){
 	var $articles = $('.article-list')
+
+	/**
+	 * AJAX 请求
+	 * --------------------------------------------------------------
+	 */
 	$.ajax({
 		url :'data/sections.json',
 		type:'get',
@@ -10,31 +21,33 @@ $(function(){
 					var str =
 						'<li class="article">'+
 							'<a href="'+item.url+'" target="_blank" class="a-container '+item.color+' fs-48">';
-
-					// 判断有没有图片
+					/*
+					 | 判断有没有图片
+					 */
 					if(item.img){
 						str += '<img src="'+item.img+'" alt="">'
 					} else {
 						str += item.title;
 					}
-
 					str = str + '</a>'+
 							'<p class="p-tip">'+item.tip+'</p>'+
 						'</li>';
 					$articles.append(str);
-				})
+				});
 
 				$articles.append('<li class="placeholder"></li>');
 			}
 		}
-	})
+	});
 
+	/**
+	 * 用户操作事件处理
+	 * --------------------------------------------------------------
+	 */
 	$(".screen").click(function() {
 		var el = $(this),
 			newone = el.clone(true);
-
 		el.before(newone);
 		$(el).remove();
 	});
-
-})
+});
